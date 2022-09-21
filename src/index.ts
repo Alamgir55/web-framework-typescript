0fe9b762b5d6d421b2eb1e18711c23a720004eb8
Alamgir55 <alamgir555hossain@gmail.com>
2022-09-21 07:24:13 -0400

    'rendering-view-nesting'

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
+import { UserEdit } from "./views/UserEdit";
 import { User } from "./models/User";
-const collection = User.buildUserCollection();
 
-collection.on("change", () => {
-  console.log(collection);
-});
+const user = User.buildUser({ name: "NAME", age: 20 });
 
-collection.fetch();
+const root = document.getElementById("root");
+
+if (root) {
+  const userEdit = new UserEdit(root, user);
+
+  userEdit.render();
+
+  console.log(userEdit);
+} else {
+  throw new Error("Root element not found");
+}
